a3af7e81b7b868dabf8f63ae62492958ecc1197a
jssPolls <jagadguru6969@gmail.com>
2017-04-01 03:05:42 +0530

    Add files via upload

---
+++ models/character.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var characterSchema = new mongoose.Schema({
+  characterId: { type: String, unique: true, index: true },
+  name: String,
+  race: String,
+  gender: String,
+  branch: String,
+  year: String,
+  url: String,
+  rating: Number,
+  bloodline: String,
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  reports: { type: Number, default: 0 },
+  random: { type: [Number], index: '2d' },
+  voted: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Character', characterSchema);
